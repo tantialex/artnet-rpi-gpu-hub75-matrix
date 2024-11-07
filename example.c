b7ea08ac501770aac67eacf67d39dc51d8054c6b
cory <cory@bitslip6.com>
2024-11-07 12:08:58 -0700

    fix merge error

--- example.c
+++ example.c
@@ -98,12 +98,8 @@ int main(int argc, char **argv)
     if (scene->shader_file == NULL) {
         pthread_create(&update_thread, NULL, render_cpu, scene);
     } else {
-<<<<<<< HEAD
-        scene->stride = 4;    // glReadPixels() returns 4 byte RGBA pixels
-=======
         printf("render shader [%s]", scene->shader_file);
         scene->stride = 4;
->>>>>>> 29cad8838ede1a34d19f34ce5f00a9e8e5bcd84e
         pthread_create(&update_thread, NULL, render_shader, scene);
     }
 
